3a7c6809204f30ec0da9b7a0fdb8535812792342
Talat Iqbal <SyedTalatIqbal@gmail.com>
2017-08-18 14:27:46 -0600

    Can search for a specific class in the directory of jars

--- src/ca/talat/main/JarReflection.java
+++ src/ca/talat/main/JarReflection.java
@@ -2,24 +2,28 @@ package ca.talat.main;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class JarReflection {
 	
-	private static String fileExtension = ".jar" ;
+	private static String fileExtension = ".jar" ; 
 
 	/**
 	 * Finds the list of all class files in a jar file and returns their fully qualified names
 	 * @param jarFilePath
 	 * @return List<String>
 	 */
-	private List<String> processClassFiles(String jarFilePath) {
+	private List<String> getClassesFromJar(String jarFilePath) {
 		List<String> classNames = new ArrayList<String>();
+		ZipInputStream zip = null;
 		try {
-			ZipInputStream zip = new ZipInputStream(new FileInputStream(jarFilePath));
+			zip = new ZipInputStream(new FileInputStream(jarFilePath));
 			for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
 			    if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
 			        String className = entry.getName().replace('/', '.'); // including ".class"
@@ -29,6 +33,12 @@ public class JarReflection {
 			return classNames;
 		} catch (Exception ex) {
 			return null;
+		} finally {
+			try {
+				zip.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
@@ -45,13 +55,69 @@ public class JarReflection {
 		for (int index=0; index<listOfFiles.length; index++) {
 			if (listOfFiles[index].isFile()) {
 				if(listOfFiles[index].getPath().endsWith(fileExtension)) {
-					classNames.addAll(processClassFiles(listOfFiles[index].getPath()));
+					classNames.addAll(getClassesFromJar(listOfFiles[index].getPath()));
 				}
 			} else if (listOfFiles[index].isDirectory()) {
 				classNames.addAll(getAllContents(listOfFiles[index].getPath()));
 			}
+		}		
+		return classNames;
+	}
+	
+	/**
+	 * Finds all the jars that contains the class name sent as a parameter
+	 * @param directoryPath
+	 * @param searchClassName
+	 * @return A map of Fully qualified class name and the jar file which contains the class name
+	 */
+	public Map<String, String> findClassesInJars(String directoryPath, String searchClassName){
+		Map<String, String> classNames = new HashMap<>();
+		File folder = new File(directoryPath);
+		File[] listOfFiles = folder.listFiles();
+		
+		for (int index=0; index<listOfFiles.length; index++) {
+			
+			File currentFileOrFolder = listOfFiles[index];
+			
+			if (currentFileOrFolder.isFile()) {
+				if(currentFileOrFolder.getPath().endsWith(fileExtension)) {
+					classNames.putAll(isClassInJar(listOfFiles[index].getPath(), searchClassName));					
+				}
+			} else if (listOfFiles[index].isDirectory()) {
+				classNames.putAll(findClassesInJars(listOfFiles[index].getPath(), searchClassName));
+			}
+		}		
+		return classNames;
+	}
+	
+	/**
+	 * Finds if the jar file contains the class name and returns the fully qualified class names
+	 * @param jarFilePath
+	 * @param className
+	 * @return The fully qualified class name
+	 */
+	private Map<String, String> isClassInJar(String jarFilePath, String className) {
+	
+		List<String> fullyQualifiedClassNames = getClassesFromJar(jarFilePath);
+		Map<String, String> result = new HashMap<String, String>();
+		
+		for (int i=0; i<fullyQualifiedClassNames.size(); i++) {
+			String fqClassName = fullyQualifiedClassNames.get(i);
+			String shortClassName = extractShortClassName(fqClassName);
+			if (shortClassName.toLowerCase().contains(className.toLowerCase())) {
+				result.put(fqClassName, jarFilePath);
+			}
 		}
 		
-		return classNames;
+		return result;
+	}
+	
+	/**
+	 * Extract the short name (final class name) from the fully qualified class name
+	 * @param fullClassName
+	 * @return String - Class Name
+	 */
+	private String extractShortClassName(String fullClassName) {
+		return fullClassName.substring(fullClassName.lastIndexOf('.'),fullClassName.length());
 	}
 }
--- src/ca/talat/main/JarReflectionRunner.java
+++ src/ca/talat/main/JarReflectionRunner.java
@@ -1,7 +1,10 @@
 package ca.talat.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class JarReflectionRunner {
 	
@@ -15,13 +18,51 @@ public class JarReflectionRunner {
 			System.out.println("Need at least one argument - directory name");
 		}
 		String directoryPath = args[0];
-		JarReflection jarReflection = new JarReflection();
-		List<String> allClassNames = new ArrayList<>();
-		allClassNames.addAll(jarReflection.getAllContents(directoryPath));
+		Scanner scanner = null;
 		
-		// Output
-		allClassNames.forEach((className) -> {
-			System.out.println(className);
-		});
+		try {			
+			while(true) {
+				System.out.println("\n\nSelect Option\n\t1.List All Classes\n\t2. Find a class\n\tAny other input to exit");
+				scanner = new Scanner(System.in);
+				int input = scanner.nextInt();
+				
+				JarReflection jarReflection = new JarReflection();
+				
+				switch(input) {
+					case 1:						
+						List<String> allClassNames = new ArrayList<>();
+						allClassNames.addAll(jarReflection.getAllContents(directoryPath));
+	
+						// Output
+						allClassNames.forEach((className) -> {
+							System.out.println(className);
+						});
+						
+						break;
+					case 2:
+						
+						System.out.print("Enter the class name: ");
+						scanner = new Scanner(System.in);
+						String inputClassName = scanner.nextLine();
+						
+						Map<String, String> allJars = new HashMap<>();
+						allJars.putAll(jarReflection.findClassesInJars(directoryPath, inputClassName));
+	
+						// Output
+						allJars.forEach((className,jarFile) -> {
+							System.out.println(className+"\t"+jarFile);
+						});
+						
+						break ;
+					default:
+						return;
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			System.out.println("Bye Bye");
+			scanner.close();
+		}	
 	}
 }
